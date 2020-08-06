682ec218510cd76cee2e94c61592e0170185cbf8
Taryn Hefner <taryn@joinit.org>
2020-08-06 18:25:36 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,8 +1,16 @@
 function basicTeenager(age) {
+  if (age === 13)
+  return "You are a teenager!"
 
 }
 
 function teenager(age) {
+  if (age >= 13) {
+    if (age <= 19) {
+        return "You are a teenager!"
+    }
+  }
+
 
 }
 
